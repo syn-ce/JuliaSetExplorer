f4df8ee7c0a7bbb07459c3e408acc4b8eb237c40
Maximilian Amthor <amthormaximilian@gmail.com>
2023-12-26 20:45:53 +0100

    first attempt at double precision; though wrong, quite interesting

--- glutils.ts
+++ glutils.ts
@@ -54,6 +54,170 @@ export const getFragmentShaderText = (nrIterations: number, z: string, c: string
     uniform vec2 xBounds;
     uniform vec2 yBounds;
     uniform float exponent;
+
+    vec2 twoSum(float a, float b)
+    {
+        float x = a + b;
+        float b_v = x - a;
+        float a_v = x - b_v;
+        float b_ro = b - b_v;
+        float a_ro = a - a_v;
+        float y = a_ro + b_ro;
+        return vec2(x,y);
+    }
+
+    vec2 split(float a, float s)
+    {
+        float c = (pow(2.0, s)+1.0)*a;
+        float a_big = c - a;
+        float a_hi = c - a_big;
+        float a_lo = a - a_hi;
+        return vec2(a_hi, a_lo);
+    }
+
+    vec2 twoProduct(float a, float b)
+    {
+        float x = a*b;
+        vec2 a_hl = split(a, 16.0); // ceil(32Bits / 2) = 16
+        vec2 b_hl = split(b, 16.0);
+        float err1 = x - a_hl.x * b_hl.x;
+        float err2 = err1 - a_hl.y * b_hl.x;
+        float err3 = err2 - a_hl.x * b_hl.y;
+        float y = a_hl.y * b_hl.y - err3;
+        return vec2(x,y);
+    }
+
+    vec2 df64_add(vec2 a, vec2 b)
+    {
+        vec2 s, t;
+        s = twoSum(a.x, b.x);
+        t = twoSum(a.y, b.y);
+        s.y += t.x;
+        s = twoSum(s.x, s.y);
+        s.y += t.y;
+        s = twoSum(s.x, s.y);
+        return s;
+    }
+
+    vec2 df64_diff(vec2 a, vec2 b)
+    {
+        return df64_add(a, b);
+    }
+
+    vec2 split(float a)
+    {
+        float t = a * 4097.0; // (1 << 12) + 1
+        float a_hi = t - (t-a);
+        float a_lo = a-a_hi;
+        return vec2(a_hi,a_lo);
+    }
+
+    vec4 splitComb(vec2 c)
+    {
+        vec2 t = c * 4097.0; // (1 << 12) + 1
+        vec2 c_hi = t-(t-c);
+        vec2 c_lo = c-c_hi;
+        return vec4(c_hi, c_lo);
+    }
+
+    vec2 twoProd(float a, float b)
+    {
+        float p = a*b;
+        vec2 aS = split(a);
+        vec2 bS = split(b);
+        float err = ((aS.x*bS.x - p) + aS.x*bS.y + aS.y*bS.x) + aS.y*bS.y;
+        return vec2(p, err);
+    }
+
+    vec2 df64_mult(vec2 a, vec2 b)
+    {
+        vec2 p = twoProd(a.x, b.x);
+        p.y += a.x * b.y;
+        p.y += a.y * b.x;
+        p = twoSum(p.x, p.y);
+        return p;
+    }
+
+    vec2 df64_div(vec2 b, vec2 a)
+    {
+        vec2 xn = vec2(1.0/a.x, 0.0);
+        vec2 yn = vec2(b.x*xn.x, 0.0);
+        float diff = (df64_diff(b, df64_mult(a,yn))).x;
+        vec2 prod = twoProd(xn.x, diff);
+
+        return df64_add(yn, prod);
+    }
+
+    vec2 df64_sqrt(vec2 a)
+    {
+        float xn = 1.0 / sqrt(a.x);
+        float yn = a.y*xn;
+        vec2 ynsqr = twoProd(yn, yn);
+        float diff = df64_diff(a, ynsqr).x; // have a look at this again, it doesn't look right
+
+        vec2 prod = twoProd(xn, diff) * 0.5;
+
+        return df64_add(vec2(yn,0), prod);
+    }
+
+    bool df64_lt(vec2 a, vec2 b)
+    {
+        return (a.x < b.x || (a.x == b.x && a.y < b.y));
+    }
+
+    vec2 df64_expTaylor(vec2 a)
+    {
+        float thresh = 1.0e-20*exp(a.x);
+        vec2 t,p,f,s,x,m;
+
+        s = df64_add(vec2(1.0,0.0), a);
+        p = df64_mult(a,a);
+        m = vec2(2.0,0.0);
+        t = p * 0.5;
+
+        while (abs(t.x) > thresh)
+        {
+            s = df64_add(s,t);
+            p = df64_mult(p,a);
+            m.x += 1.0;
+            f = df64_mult(f,m);
+            t = df64_div(p,f);
+        }
+
+        return df64_add(s,t);
+    }
+
+    void main()
+    {
+        // Convert position on screen to position in coordinate system, as previously done by Viewport
+        float x = gl_FragCoord.x / screenResolution.x * (xBounds.y - xBounds.x) + xBounds.x;
+        float y = gl_FragCoord.y / screenResolution.y * (yBounds.y - yBounds.x) + yBounds.x;
+        vec2 z = ${z};
+        vec2 z_real = vec2(z.x, 0.0);
+        vec2 z_imag = vec2(z.y, 0.0);
+        vec2 c = ${c};
+        vec2 c_real = vec2(c.x, 0.0);
+        vec2 c_imag = vec2(c.y, 0.0);
+        for (float i = 0.0; i < ${nrIterations}.0; i++)
+        {
+            z_real = df64_add(df64_diff(df64_mult(z_real,z_real), df64_mult(z_imag,z_imag)), c_real);
+            z_imag = df64_add(df64_add(df64_mult(z_real,z_real),z_imag), c_imag);
+            //z = vec2(z.x*z.x - z.y*z.y, (z.x+z.x) * z.y) + c; 
+
+            if (df64_lt(vec2(escapeRadius,0.0), df64_add(df64_mult(z_real,z_real), df64_mult(z_imag,z_imag))))
+            {
+                //float colVal = i + 1. - log(log(sqrt(z.x*z.x + z.y*z.y))) / log(2.0);
+                //colVal = colVal / ${nrIterations + 1}.0;
+                //myOutputColor= vec4(rgbColor * colVal, 1.0);
+                float colVal = i / ${nrIterations + 1}.0;
+                myOutputColor = vec4(rgbColor * colVal, 1.0);
+                return;
+            }
+        }
+        myOutputColor = vec4(0.0, 0.0, 0.0, 1.0);
+    }
+
+
     
     vec2 complexExp(float x, float y) // c = x + i*y
     {
@@ -67,7 +231,7 @@ export const getFragmentShaderText = (nrIterations: number, z: string, c: string
         return vec2(real, imag);
     }
 
-    void main()
+    void main2()
     {
         // Convert position on screen to position in coordinate system, as previously done by Viewport
         float x = gl_FragCoord.x / screenResolution.x * (xBounds.y - xBounds.x) + xBounds.x;
